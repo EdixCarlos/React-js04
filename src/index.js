e159d8b0d2aa68623a613a1b74fb35d9b911b9be
Edix Carlos <edix.carlos@tecsup.edu.pe>
2021-03-31 22:20:24 -0500

    punto2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0.0)
+  const [positive, setPositive] = useState(0.0)
+  const votoGood = () => {
+    setGood(good+1)
+    averageS()
+    positiveS()
+    allS()
+  }
+  const votoNeutral = () => {
+    setNeutral(neutral+1)
+    averageS()
+    positiveS()
+    allS()
+  }
+  const votoBad = () => {
+    setBad(bad+1)
+    averageS()
+    positiveS()
+    allS()
+  }
+  const averageS = () => {
+    setAverage((good + neutral - neutral - bad)/(good + neutral + bad))
+  }
+  const positiveS = () => {
+    setPositive((good/(good + neutral + bad))*100)
+  }
+  const allS = () => {
+    setAll(good + neutral + bad)
+  }
 
   return (
     <div className="App">
@@ -13,7 +43,10 @@ const App = () => {
       <button onClick={votoGood}>Good</button>
       <button onClick={votoNeutral}>Neutral</button>
       <button onClick={votoBad}>Bad</button>
-      
+      <p>
+        good {good} <br/>neutra {neutral}<br/>bad {bad}<br/>
+      all {all}<br/> average {average}<br/>positive {positive}%
+      </p>
     </div>
   )
 }
